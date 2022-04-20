6e3483b8e5bae4df4970d1ed90b600f1389b93e9
anurag47103 <anuragsharma47103@gmail.com>
2022-04-20 16:43:00 +0530

    -delete function implemented

--- app/src/main/java/com/learningandroid/journal/ui/JournalRecyclerAdapter.java
+++ app/src/main/java/com/learningandroid/journal/ui/JournalRecyclerAdapter.java
@@ -1,18 +1,38 @@
 package com.learningandroid.journal.ui;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.learningandroid.journal.JournalListActivity;
 import com.learningandroid.journal.R;
 import com.learningandroid.journal.model.Journal;
 import com.squareup.picasso.Picasso;
@@ -22,6 +42,13 @@ import java.util.List;
 public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecyclerAdapter.ViewHolder> {
     private Context context;
     private List<Journal> journalList;
+
+    private static Dialog infoDialog;
+    private CardView delete;
+    private ProgressBar progressBarDelete;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Journal");
+
 
     public JournalRecyclerAdapter(Context context, List<Journal> journalList) {
         this.context = context;
@@ -77,6 +104,7 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
 
             }
         });
+
     }
 
     @Override
@@ -106,8 +134,68 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
             name = itemView.findViewById(R.id.journal_row_username);
             shareButton = itemView.findViewById(R.id.journal_row_share_button);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Log.d("check", "onLongClick: " + "very loong");
+                    infoDialog = new Dialog(context);
+                    infoDialog.setContentView(R.layout.delete_popup);
+                    infoDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    infoDialog.show();
+
+                    delete = infoDialog.findViewById(R.id.card_delete);
+                    progressBarDelete = infoDialog.findViewById(R.id.progress_delete);
+
+                    delete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            progressBarDelete.setVisibility(View.VISIBLE);
+//                            deleteJournal(getAdapterPosition());
+                            final int position = getAdapterPosition();
+                            final Journal journal = journalList.get(position);
+                            final String userId = journal.getUserId();
+                            final String url=journal.getImageUri();
+                            final Timestamp timeAdded = journal.getTimeAdded();
+
+                            collectionReference
+                                    .whereEqualTo("userId", userId)
+                                    .whereEqualTo("timeAdded", timeAdded)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                                            for (QueryDocumentSnapshot snapshot : value) {
+//                                                Log.d(TAG, snapshot.getId());
+                                                snapshot.getReference().delete()
+                                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                            @Override
+                                                            public void onSuccess(Void unused) {
+                                                                progressBarDelete.setVisibility(View.INVISIBLE);
+                                                                Toast.makeText(context, "Journal Deleted", Toast.LENGTH_SHORT).show();
+
+                                                                final Intent intent = new Intent(context, JournalListActivity.class);
+                                                                context.startActivity(intent);
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                    });
+                            StorageReference photoRef = FirebaseStorage.getInstance().getReferenceFromUrl(url);
+                            photoRef.delete();
 
 
+                        }
+                    });
+                    return true;
+                }
+            });
         }
     }
 }
